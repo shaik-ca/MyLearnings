f2bcdfdef1eb54ba7ec24b2aeff4ec1e5d7f65c1
rabbani <rabbani.shaik.mca@gmail.com>
2024-09-06 07:44:10 +0300

    List

---
+++ Python/Lists.py
@@ -0,0 +1,52 @@
+# myList=[1,2,3,4,5,6,7,8,9,10]
+
+# print(myList)
+# #list slicing..
+# print(myList[0:5:]) 
+# print(myList[::1]) 
+# print(myList[::-1]) 
+# #diaply list items bying for loop
+# for i in myList:
+#     print(i)
+
+#List operations and mehtods
+# a=[1,2,3,4,5]
+# b=[2,2,3,4,5]
+
+# print(a>b)
+# print(a+b)
+# print(a==b)
+# print(len(a))
+
+'''Append List'''
+# myList1=list()
+# while(True):
+#     n=eval(input("Please enter a number: "))
+#     if(n==-1):
+#         break
+#     myList1.append(n)
+#inset a new number.
+# myList1.insert(0,1000)
+# print(myList1)
+# _popNum=myList1[1]
+# print(_popNum)
+
+# print(myList1.remove(1))
+# print(myList1)
+# print(myList1.clear())
+
+# n=45554
+# a=list(str(n))
+# b=a[::-1]
+# print(a,b)
+# if (b==a):
+#     print("palindrome")
+# else:
+#     print("note palindrome")
+
+'''Extend List'''
+a=[1,2,3,4,5]
+b=[6,7,8,9]
+a.extend(b)
+print(a)
+
